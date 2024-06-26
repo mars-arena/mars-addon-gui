1af9d3bbee516fe4761292ea39837ffe48c005ee
nazarpunk <nazarpunk@gmail.com>
2024-06-26 20:57:50 +0300

    Add test Anal

---
+++ gen/download.mjs
@@ -0,0 +1,35 @@
+import * as fs from 'fs'
+import * as https from 'https'
+import path from 'path'
+
+const download = (url, dest, trim = false) => {
+    const file = fs.createWriteStream(dest)
+    https.get(url, function (response) {
+        response.pipe(file)
+        file.on('finish', () => {
+            file.close(() => {
+                fs.readFile(dest, {encoding: 'utf8'}, (err, data) => {
+                    if (err) return console.log(err)
+
+                    let result = data
+
+                    if (trim) result = result.replace(/\r\n/g, '\n')
+                        .replace(/[^\S\r\n]{2,}/g, ' ')
+                        .replace(/\n[^\S\r\n]+/g, '\n')
+
+                    fs.writeFile(dest, result, {encoding: 'utf8'}, err => {
+                        if (err) return console.log(err)
+                    })
+                })
+            })
+        })
+    }).on('error', function (err) {
+        console.log(err)
+    })
+}
+
+// https://github.com/UnryzeC/UjAPI/tree/main/uJAPIFiles
+
+const dest = path.join('..', 'src', 'common.j')
+
+download('https://raw.githubusercontent.com/UnryzeC/UjAPI/main/uJAPIFiles/common.j', dest, true)
--- gen/gen.mjs
+++ gen/gen.mjs
@@ -10,8 +10,15 @@ const UI = path.join(root, 'src', 'UI')
 rmdir(UI)
 cpdir(path.join(root, 'src', 'umswe', 'UI'), UI)
 
-const MiscData = path.join(UI, 'MiscData.txt')
+//const MiscData = path.join(UI, 'MiscData.txt')
 
+//const commonj = path.join('..', 'src', 'common.j')
+
+/**
+ * @param {string} filePath
+ * @param {string} s
+ * @param {string} part
+ */
 const append = (filePath, s, part) => {
     const data = fs.readFileSync(filePath)
     const h = `[${part}]`
@@ -19,5 +26,22 @@ const append = (filePath, s, part) => {
     fs.writeFileSync(filePath, Buffer.concat([data.subarray(0, pos), Buffer.from('\n' + s), data.subarray(pos)]))
 }
 
+const TriggerData = path.join(UI, 'TriggerData.txt')
+const TriggerStrings = path.join(UI, 'TriggerStrings.txt')
+const WorldEditStrings = path.join(UI, 'WorldEditStrings.txt')
 
-//append(MiscData, 'a=b', 'Camera')
+append(TriggerData, 'TC_UJAPI=WESTRING_TRIGCAT_UJAPI,ReplaceableTextures\\WorldEditUI\\Actions-UnitSelection', 'TriggerCategories')
+append(WorldEditStrings, 'WESTRING_TRIGCAT_UJAPI=UjAPI', 'WorldEditStrings')
+
+append(TriggerData, `
+AnalCunt=0,unit,integer,permanentoption
+_AnalCunt_Defaults=GetTriggerUnit,0,PermanentPerm
+_AnalCunt_Category=TC_UJAPI
+`, 'TriggerActions')
+
+append(TriggerStrings, `
+AnalCunt=Anal Cunt
+AnalCunt="Set Anal of ",~Hero," to ",~Integer,", the effect will be ",~Permanent
+AnalCuntHint=Changes the hero's agility [UMSWE]
+`, 'TriggerActionStrings')
+
