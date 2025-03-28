d06f17e2c8c3a85d7a166479666cf2049bcdc788
nazarpunk <nazarpunk@gmail.com>
2025-03-28 11:04:01 +0200

    add java

---
+++ dist/copy.bat
@@ -0,0 +1,5 @@
+call variables.bat
+
+set target="umswe.mpq"
+
+copy %target% "C:\Users\nazarpunk\Desktop\Jass New Gen Pack Rebuild\umswe\umswe.mpq"
--- src/main/kotlin/io/github/warraft/ujapi_gui/Main.kt
+++ src/main/kotlin/io/github/warraft/ujapi_gui/Main.kt
@@ -1,5 +1,23 @@
 package io.github.warraft.ujapi_gui
 
-fun main(){
-    println("Hello World!")
+import io.github.warraft.mpq4j.MPQ4J
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardCopyOption
+
+fun main() {
+    val umswe = Paths.get("dist", "umswe.mpq")
+    Files.copy(Paths.get("data", "umswe.mpq"), umswe, StandardCopyOption.REPLACE_EXISTING)
+
+    println(umswe)
+
+    val mpq = MPQ4J(umswe)
+
+    val folder = Paths.get("data", "umswe", "UI").toFile()
+    for (file in folder.listFiles()!!) {
+        mpq.insertFile("UI/${file.name}", file, true)
+    }
+
+    mpq.close()
+
 }
