519d265668cd72d1c3a16df8896fe1f771bcea15
Illia Korzhov <illia.korzhov.ua@gmail.com>
2025-09-25 19:25:21 +0000

    fix fix

--- gen/utils/file-data-append.mjs
+++ gen/utils/file-data-append.mjs
@@ -6,25 +6,8 @@ import fs from 'fs'
  * @param {string} part
  */
 export default (filePath, s, part) => {
-    const data = fs.readFileSync(filePath, 'utf8')
-    const lines = data.split('\n')
+    const data = fs.readFileSync(filePath)
     const h = `[${part}]`
-
-    // Найти начало секции
-    const startIndex = lines.findIndex(line => line.startsWith(h))
-    if (startIndex === -1) {
-        throw new Error(`Section [${part}] not found in file`)
-    }
-
-    // Найти конец секции или конец файла
-    let endIndex = startIndex + 1
-    while (endIndex < lines.length && !lines[endIndex].startsWith('[')) {
-        endIndex++
-    }
-
-    // Добавить строку в конец секции и пустую строку после неё
-    lines.splice(endIndex, 0, s, '')
-
-    // Записать обновлённые данные обратно в файл
-    fs.writeFileSync(filePath, lines.join('\n'))
+    const pos = data.indexOf(h) + h.length
+    fs.writeFileSync(filePath, Buffer.concat([data.subarray(0, pos), Buffer.from('\n' + s), data.subarray(pos)]))
 }
