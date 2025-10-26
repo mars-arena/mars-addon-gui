b9bce9a54c06ab283889f5fef354796213e0b974
Illia Korzhov <illia.korzhov.ua@gmail.com>
2025-10-26 18:38:49 +0000

    at the end

--- gen/utils/file-data-append.mjs
+++ gen/utils/file-data-append.mjs
@@ -6,8 +6,24 @@ import fs from 'fs'
  * @param {string} part
  */
 export default (filePath, s, part) => {
-    const data = fs.readFileSync(filePath)
-    const h = `[${part}]`
-    const pos = data.indexOf(h) + h.length
-    fs.writeFileSync(filePath, Buffer.concat([data.subarray(0, pos), Buffer.from('\n' + s), data.subarray(pos)]))
+	const data = fs.readFileSync(filePath)
+	const h = `[${part}]`
+	const start = data.indexOf(h)
+	if (start === -1) return
+
+	const sectionStart = start + h.length
+
+	const n1 = data.indexOf('\n\n[', sectionStart)
+	const n2 = data.indexOf('\r\n\r\n[', sectionStart)
+	let sectionEnd = -1
+	if (n1 === -1 && n2 === -1) sectionEnd = data.length
+	else if (n1 === -1) sectionEnd = n2
+	else if (n2 === -1) sectionEnd = n1
+	else sectionEnd = Math.min(n1, n2)
+
+	fs.writeFileSync(filePath, Buffer.concat([
+		data.subarray(0, sectionEnd),
+		Buffer.from('\n' + s),
+		data.subarray(sectionEnd)
+	]))
 }
